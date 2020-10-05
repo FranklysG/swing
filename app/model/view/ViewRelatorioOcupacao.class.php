6f0c6fb88e3d909f8d2239991045694b8ba248cf
franklys <tunele095@gmail.com>
2020-10-05 16:16:56 -0300

    adjusts on view to relatorio of ocupacao

--- app/model/view/ViewRelatorioOcupacao.class.php
+++ app/model/view/ViewRelatorioOcupacao.class.php
@@ -6,7 +6,7 @@
 class ViewRelatorioOcupacao extends TRecord
 {
     const TABLENAME = 'view_relatorio_ocupacao';
-    const PRIMARYKEY= 'res_id';
+    const PRIMARYKEY= 'id';
     const IDPOLICY =  'max'; // {max, serial}
     
     
@@ -16,16 +16,15 @@ class ViewRelatorioOcupacao extends TRecord
     public function __construct($id = NULL, $callObjectLoad = TRUE)
     {
         parent::__construct($id, $callObjectLoad);
-        parent::addAttribute('res_status');
-        parent::addAttribute('res_dtcadastro');
-        parent::addAttribute('m_res_id');
-        parent::addAttribute('m_res_n_quarto');
-        parent::addAttribute('m_res_valor');
-        parent::addAttribute('con_id');
-        parent::addAttribute('prod_id');
-        parent::addAttribute('prod_nome');
-        parent::addAttribute('prod_valor');
-        parent::addAttribute('prod_qtd');
+        parent::addAttribute('n_quarto');
+        parent::addAttribute('valor_quarto');
+        parent::addAttribute('valor_consumo');
+        parent::addAttribute('status');
+        parent::addAttribute('dtcadastro');
+        parent::addAttribute('entrada_id');
+        parent::addAttribute('valor_venda_uni');
+        parent::addAttribute('produto_id');
+        parent::addAttribute('produto_nome');
     }
 
 
--- app/view/ViewRelatorioOcupacaoReport.class.php
+++ app/view/ViewRelatorioOcupacaoReport.class.php
@@ -72,6 +72,7 @@ class ViewRelatorioOcupacaoReport extends TPage
             
             // get the form data into an active record
             $data = $this->form->getData();
+            
             $this->form->validate();
             
             $repository = new TRepository('ViewRelatorioOcupacao');
@@ -79,16 +80,16 @@ class ViewRelatorioOcupacaoReport extends TPage
 
             if ((isset($data->date_ini) and $data->date_ini) and (isset($data->date_end) and $data->date_end))
             {
-                $criteria->add(new TFilter('date(res_dtcadastro)', 'between', "{$data->date_ini}", "{$data->date_end}"));
+                $criteria->add(new TFilter('dtcadastro', 'between', "{$data->date_ini}", "{$data->date_end}"));
             }
 
            
-            $objects = $repository->load($criteria);
+            $objects = $repository->load($criteria, FALSE);
             $format  = $data->output_type;
             
             if ($objects)
             {
-                $widths = array(120,80,80,120,60,80);
+                $widths = array(80,80,80,80,160,100);
                 
                 switch ($format)
                 {
@@ -110,7 +111,7 @@ class ViewRelatorioOcupacaoReport extends TPage
                 $tr->addStyle('title', 'Arial', '11', 'B',   '#ffffff', '#A3A3A3');
                 $tr->addStyle('datap', 'Arial', '11', 'B',    '#000000', '#EEEEEE');
                 $tr->addStyle('datai', 'Arial', '11', 'B',    '#000000', '#ffffff');
-                $tr->addStyle('header', 'Arial', '14', '',   '#ffffff', '#6B6B6B');
+                $tr->addStyle('header', 'Arial', '14', 'B',   '#ffffff', '#6B6B6B');
                 // $tr->addStyle('footer', 'Arial', '11', 'B',  '#000000', '#A3A3A3');
                 $tr->addStyle('footer', 'Arial', '11', 'B',  '#232323', '#D0D0D0');
                 
@@ -120,90 +121,85 @@ class ViewRelatorioOcupacaoReport extends TPage
                 
                 // add titles row
                 $tr->addRow();
-                // $tr->addCell('Res Id', 'right', 'title');
-                // $tr->addCell('Res Status', 'left', 'title');
-                $tr->addCell('Data Ocupação', 'left', 'title');
-                // $tr->addCell('M Res Id', 'right', 'title');
-                $tr->addCell('N° Quarto', 'left', 'title');
-                $tr->addCell('Valor', 'left', 'title');
-                // $tr->addCell('Cos Id', 'right', 'title');
-                // $tr->addCell('Prod Id', 'right', 'title');
-                $tr->addCell('Prod. Consumido', 'left', 'title');
-                $tr->addCell('Qtd', 'left', 'title');
-                $tr->addCell('Valor', 'left', 'title');
+                $tr->addCell('DATA', 'left', 'title');
+                $tr->addCell('N° QTO', 'left', 'title');
+                $tr->addCell('VALOR', 'left', 'title');
+                $tr->addCell('CONSUMO', 'left', 'title');
+                $tr->addCell('NOME', 'left', 'title');
+                $tr->addCell('VL UNI', 'left', 'title');
 
-                
                 // controls the background filling
-                $colour= FALSE;
-                $sum_valores_reserva_dia = 0;
-                $sum_valores_reserva_total = 0;
+                $colour = FALSE;
                 $date = null;
-                $m_res_id = null;
+                $id = null;
+
                 // data rows
                 foreach ($objects as $object)
                 {
-                    
                     if(is_null($date)){
-                        $date = $object->res_dtcadastro;
+                        $date = $object->dtcadastro;
                         $tr->addRow();
-                        $tr->addCell("Ocupações do dia ".Convert::toDateBR($object->res_dtcadastro) , 'left', 'footer', 6);
+                        $tr->addCell("Ocupações do dia ".Convert::toDateBR($object->dtcadastro) , 'left', 'footer', 6);
                     }
 
                     $style = $colour ? 'datap' : 'datai';
                     
                     // confiro se a data atual é diferente da anterior pra saber
                     // quando pular a linha e quando não pular
-                    if($object->res_dtcadastro != $date){
+                    if($object->dtcadastro != $date){
                         // somatorio das reserva daquele dia
                         $tr->addRow();
                         $tr->addCell('Valor total', 'left', 'footer', 3);
-                        $tr->addCell(Convert::toMonetario($sum_valores_reserva_dia), 'right', 'footer', 3);
+                        $tr->addCell(Convert::toMonetario($sum_valores_dia), 'right', 'footer', 3);
                         $tr->addRow();
                         
-                        $sum_valores_reserva_dia = 0;
-                        $date = $object->res_dtcadastro;
+                        $sum_valores_dia = 0;
+                        $date = $object->dtcadastro;
                         
                         $tr->addRow();
-                        $tr->addCell("Ocupações do dia ".Convert::toDateBR($object->res_dtcadastro) , 'left', 'footer', 6);
+                        $tr->addCell("Ocupações do dia ".Convert::toDateBR($object->dtcadastro) , 'left', 'footer', 6);
                     }
 
                     // conferindo se o id do mapa_reserva é igual ao anterior 
                     // pra não duplicar os valores na hora de gerar o relatorio
                     // tambem já estou somando os valores de cada dia, e o valor completo do quartos
                     $valor_quarto = '';
-                    if($object->m_res_id != $m_res_id){
-                        $valor_quarto = $object->m_res_valor;
-                        $sum_valores_reserva_total += $object->m_res_valor;
-                        $sum_valores_reserva_dia += $object->m_res_valor;
-                        $m_res_id = $object->m_res_id;
+                    $valor_consumo = '';
+                    if($object->id != $id){
+                        $valor_quarto = $object->valor_quarto;
+                        $valor_consumo = $object->valor_consumo;
+                        $valor_quarto_consumo = ($object->valor_quarto + $object->valor_consumo);
+                        $sum_valores_reserva_total += $valor_quarto_consumo;
+                        $sum_valores_dia += $valor_quarto_consumo;
+                        $id = $object->id;
                     }
                     
-                    
                     $tr->addRow();
-                    $tr->addCell(Convert::toDateBR($object->res_dtcadastro), 'left', $style);
-                    $tr->addCell($object->m_res_n_quarto, 'right', $style);
-                    $tr->addCell((!empty($valor_quarto))? Convert::toMonetario($valor_quarto): ' ', 'right', $style);
-                    $tr->addCell($object->prod_nome, 'right', $style);
-                    $tr->addCell($object->prod_qtd, 'right', $style);
-                    $tr->addCell(($object->prod_valor)? Convert::toMonetario($object->prod_valor) : ' ', 'right', $style);
-                                           
+                    $tr->addCell(Convert::toDateBR($object->dtcadastro), 'left', $style);
+                    $tr->addCell($object->n_quarto, 'right', $style);
+                    $tr->addCell((!empty($valor_quarto))?Convert::toMonetario($valor_quarto) : '', 'right', $style);
+                    $tr->addCell((!empty($valor_consumo))?Convert::toMonetario($valor_consumo) : '', 'right', $style);
+                    $tr->addCell(mb_strimwidth($object->produto_nome, 0, 21, "..."), 'left', $style);
+                    $tr->addCell(Convert::toMonetario($object->valor_venda_uni), 'right', $style);
+
+                    
                     $colour = !$colour;
                 }
                 
                 // adicionando a ultima linha do relatorio
                 $tr->addRow();
                 $tr->addCell('Valor total', 'left', 'footer', 3);
-                $tr->addCell(Convert::toMonetario($sum_valores_reserva_dia), 'right', 'footer', 3);
+                $tr->addCell(Convert::toMonetario($sum_valores_dia), 'right', 'footer', 3);
                 
-                 // valor total das ocupações
-                 $tr->addRow();
-                 $tr->addCell('valor total do relatorio', 'left', 'footer', 3);
-                 $tr->addCell(Convert::toMonetario($sum_valores_reserva_total), 'right', 'footer', 3);
+                // valor total das ocupações
+                $tr->addRow();
+                $tr->addRow();
+                $tr->addCell('valor total do relatorio', 'left', 'footer', 3);
+                $tr->addCell(Convert::toMonetario($sum_valores_reserva_total), 'right', 'footer', 3);
 
                 // footer row
                 $tr->addRow();
                 $tr->addCell('Relatorio de '.date('d/m/Y h:i:s'), 'center', 'footer', 6);
-                
                 
                 // stores the file
                 if (!file_exists("app/output/relatorio_ocupacao.{$format}") OR is_writable("app/output/relatorio_ocupacao.{$format}"))
@@ -229,6 +225,7 @@ class ViewRelatorioOcupacaoReport extends TPage
                 new TMessage('error', 'Nenhum Registro entre essas datas');
                 new TMessage('error', 'data inicial não pode ser maior que data final');
             }
+    
     
             // fill the form with the active record data
             $this->form->setData($data);
